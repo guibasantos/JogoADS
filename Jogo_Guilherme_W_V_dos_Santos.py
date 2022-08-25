d83efb552bb982778db14b18b23c67b398da8c76
Guilherme <gwvsantos@hotmail.com>
2022-08-24 21:29:11 -0300

    Condição de Vitória

--- Jogo_Guilherme_W_V_dos_Santos.py
+++ Jogo_Guilherme_W_V_dos_Santos.py
@@ -87,6 +87,7 @@ while True:
                 verde = 0
                 amarelo = 0
                 vermelho = 0
+                lista_dados = lista_dados
                 break
             elif tiro != 3:
                 deci = str(input('Você deseja continuar jogando? [S/N] ')).upper().strip()[0]
@@ -95,8 +96,12 @@ while True:
                     verde = 0
                     amarelo = 0
                     vermelho = 0
+                    lista_dados = lista_dados
                     break
                 else:
                     verde = 0
                     amarelo = 0
                     vermelho = 0
+        if cerebro == 13:
+            print(f'Parabéns {lista_nomes[cont]} você venceu o jogo!')
+            break
